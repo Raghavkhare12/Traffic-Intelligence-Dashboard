52699fcb4a345cfe81cfae49db33e57907b91a12
Raghavkhare12 <raghav122006@gmail.com>
2026-06-19 15:29:38 +0530

    Update CORS for deployment

--- backend/app/main.py
+++ backend/app/main.py
@@ -165,9 +165,7 @@ def monthly_events(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000"
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
